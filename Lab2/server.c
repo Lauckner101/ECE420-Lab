1f725af9547218cc4b7df0fb58008128579dc529
Pratham Sitoula <psitoula2003@gmail.com>
2026-01-28 12:55:25 -0700

    added args required for server

--- Lab2/server.c
+++ Lab2/server.c
@@ -1,2 +1,23 @@
 #include "common.h"
 #include "timer.h"
+#include <stdlib.h>
+
+
+
+
+int main(int argc, char* argv[]){
+  int N, i;
+  char * server_ip;
+  int server_port;
+
+  if (argc != 4) {
+    printf("Invalid arguments: server <array_size> <ip> <port>\n");
+    return EXIT_FAILURE;
+  }
+
+  N = atoi(argv[1]);
+  server_ip = argv[2];
+  server_port = atoi(argv[3]);
+
+  return EXIT_SUCCESS;
+}
