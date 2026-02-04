e23b8236539be6ec8d837f5ad32d837a9269e78d
Pratham Sitoula <psitoula2003@gmail.com>
2026-02-03 20:35:54 -0700

    Added remaining func

--- Lab2/server.c
+++ Lab2/server.c
@@ -13,8 +13,9 @@
 char **theArray;                    // array of strings
 pthread_rwlock_t *array_locks;      // one RW lock per array index
 int ARRAY_SIZE;
-
-
+int cur_idx = 0;
+double latency_array[COM_NUM_REQUEST];
+pthread_mutex_t time_lock;
 
 
 void *HandleClient(void *arg) {
@@ -32,16 +33,15 @@ void *HandleClient(void *arg) {
 
     double start, end;
 
+    GET_TIME(start);
     if (rq.is_read == 1) {
         // read
-        GET_TIME(start);
         pthread_rwlock_rdlock(&array_locks[rq.pos]);
         getContent(reply, rq.pos, theArray);
         pthread_rwlock_unlock(&array_locks[rq.pos]);
 
     } else {
         // write
-        GET_TIME(start);
         pthread_rwlock_wrlock(&array_locks[rq.pos]);
         setContent(rq.msg, rq.pos, theArray);
         getContent(reply, rq.pos, theArray);
@@ -51,6 +51,10 @@ void *HandleClient(void *arg) {
     /* Send response */
     write(client_fd, reply, COM_BUFF_SIZE);
     GET_TIME(end);
+    pthread_mutex_lock(&time_lock);
+    latency_array[cur_idx] = end - start;
+    cur_idx++;
+    pthread_mutex_unlock(&time_lock);
     close(client_fd);
 
     return NULL;
@@ -72,7 +76,7 @@ int main(int argc, char *argv[]) {
     theArray = malloc(ARRAY_SIZE * sizeof(char *));
     array_locks = malloc(ARRAY_SIZE * sizeof(pthread_rwlock_t));
     pthread_t thread_handles[COM_NUM_REQUEST];
-
+    pthread_mutex_init(&time_lock, NULL);
     for (int i = 0; i < ARRAY_SIZE; i++) {
         theArray[i] = malloc(COM_BUFF_SIZE);
         snprintf(theArray[i], COM_BUFF_SIZE,
@@ -90,7 +94,7 @@ int main(int argc, char *argv[]) {
 
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = server_port;
+    addr.sin_port = htons(server_port);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
@@ -103,6 +107,7 @@ int main(int argc, char *argv[]) {
 
     while (1) {
         int i = 0;
+        cur_idx = 0;
         while (i < COM_NUM_REQUEST) {
             int client_fd = accept(server_fd, NULL, NULL);
             if (client_fd < 0) {
@@ -116,6 +121,7 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < COM_NUM_REQUEST; i++) {
             pthread_join(thread_handles[i], NULL);
         }
+        saveTimes(latency_array, COM_NUM_REQUEST);
     }
 
     close(server_fd);
@@ -127,6 +133,7 @@ int main(int argc, char *argv[]) {
     
     free(theArray);
     free(array_locks);
+    pthread_mutex_destroy(&time_lock);
 
     return 0;
 }
